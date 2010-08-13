53d423504799aac08527e4a44c29fca511672de8
Patrick Hall <pat@amundo.com>
2010-08-13 04:09:09 -0700

    Another Uyghur script

---
+++ js/ug-Phon-table.js
@@ -0,0 +1,22 @@
+var ug_Phon = [
+
+  [ "ch", '\u02A7'], 	// ʧ LATIN SMALL LETTER TESH DIGRAPH (U+02A7)
+  [ "ng", '\u014b'], 	// ŋ LATIN SMALL LETTER ENG (U+014B)
+  [ "i-", '\u0268'], 	// ɨ LATIN SMALL LETTER I WITH STROKE (U+0268)
+  [ "0",  '\u0325'], 	// COMBINING RING BELOW (U+0325)
+  [ ":",  '\u02D0'], 	// ː MODIFIER LETTER TRIANGULAR COLON (U+02D0)
+  [ "@",  '\u0259'], 	// ə LATIN SMALL LETTER SCHWA (U+0259 )
+  [ "A",  '\u00E6'], 	// æ LATIN SMALL LETTER AE (U+00E6)
+  [ "E",  '\u025b'], 	// ɛ LATIN SMALL LETTER OPEN E (U+025B)
+  [ "G",  '\u0262'], 	// ɢ LATIN LETTER SMALL CAPITAL G (U+0262)
+  [ "I",  '\u026A'], 	// ɪ LATIN LETTER SMALL CAPITAL I (U+026A)
+  [ "J",  '\u02A4'], 	// ʤ  LATIN SMALL LETTER DEZH DIGRAPH (U+02A4)
+  [ "O",  '\u00f8'], 	// ø LATIN SMALL LETTER O WITH STROKE (U+00F8)
+  [ "R",  '\u0281'], 	// ʁ LATIN LETTER SMALL CAPITAL INVERTED R (U+0281)
+  [ "S",  '\u0283'], 	// ʃ LATIN SMALL LETTER ESH (U+0283)
+  [ "X",  '\u03C7'], 	// χ GREEK SMALL LETTER CHI (U+03C7)
+  [ "X",  '\u03C7'], 	// χ GREEK SMALL LETTER CHI (U+03C7)
+  [ "Z",  '\u0292'], 	// ʒ LATIN SMALL LETTER EZH (U+0292)
+  [ "~",  '\u0303'] 	// COMBINING TILDE (U+0303)
+
+];
