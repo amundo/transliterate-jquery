5a3206fec2f21e0bc67f69b9597b31147759aed3
Patrick Hall <pat@amundo.com>
2010-08-12 22:33:58 -0700

    generating an input automatically; doesn't work in safari; css borked on chrome

--- js/jquery.transliterate.js
+++ js/jquery.transliterate.js
@@ -4,6 +4,34 @@ jquery.transliterate.js -- jQuery based transliteration module
 
 MIT license 
 
+use me like this:
+
+    $('input[lang=chr]').transliterate({tableFile: 'chr.js'})
 
 
 */
+
+(function($){
+
+  $.fn.transliterate = function(options){
+
+    return this.each(function(){
+
+      var opts = $.extend({}, $.fn.transliterate.defaults, options);
+
+      $('<input/>', {
+        class: opts.sourceInputClass,
+        //placeholder: 'type here...'
+      })
+      .insertBefore( $(this) )
+      .focus()
+
+    })
+
+    $.fn.transliterate.defaults = { 
+      // table, caseSensitive
+    };
+
+  }
+
+})(jQuery)
