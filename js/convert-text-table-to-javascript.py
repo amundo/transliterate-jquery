76b2d30564e0575347e002e3950553e014c7cdee
Patrick Hall <pat@amundo.com>
2010-08-12 23:26:49 -0700

    the longest substitution "befores" should run first

--- js/convert-text-table-to-javascript.py
+++ js/convert-text-table-to-javascript.py
@@ -3,12 +3,15 @@
 
 import sys
 import codecs
+from operator import itemgetter
 
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 source_file =  sys.argv[1]
 
 lines = open(source_file).read().decode('utf-8').strip().splitlines()
 pairs = [line.strip().split() for line in lines]
+temp = sorted([(len(a), a, b) for a,b in pairs], reverse=True)
+pairs = [(y,z) for x, y, z in temp]
 
 lang_code = source_file.split('-')[0] # i'm looking at you.
 
